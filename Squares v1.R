cf0fe362b4b882873726bb09cddf3bc8bf6e15dd
Kaltenburg1 <daniel.spring@monash.edu>
2015-05-16 12:21:23 +1000

    printed squares of numbers 1-5 with 2 methods

---
+++ Squares v1.R
@@ -0,0 +1,7 @@
+#This script prints the square of the numbers 1,..,5 in ascending order.
+print(1^2)
+print(2^2)
+print(3^2)
+print(4^2)
+print(5^2)
+
---
+++ Squares v2 loop.R
@@ -0,0 +1,6 @@
+# This script prints the square of the numbers 1,..,5 in ascending order using a loop.
+print("print out squares of 5 numbers, from 1 to 5")
+for (x in 1:5)
+{
+  print(x^2)
+}
